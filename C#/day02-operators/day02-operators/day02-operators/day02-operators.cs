bdbeb0323c7c8b6485c8e6c1e60d9505e1c7a270
Scott Anecito <sanecito@users.noreply.github.com>
2016-08-22 20:44:13 -0700

    Did Day 2 - Operators

---
+++ C#/day02-operators/day02-operators/day02-operators/day02-operators.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        decimal mealCost = decimal.Parse(Console.ReadLine());
+        decimal tipPercent = Int32.Parse(Console.ReadLine());
+        decimal taxPercent = Int32.Parse(Console.ReadLine());
+
+        decimal tipTotal = (decimal)(mealCost * (tipPercent / 100));
+        decimal taxTotal = (decimal)(mealCost * (taxPercent / 100));
+
+        decimal totalCost = mealCost + tipTotal + taxTotal;
+
+        Console.WriteLine("The total meal cost is " + Decimal.Round(totalCost) + " dollars.");
+    }
+}
